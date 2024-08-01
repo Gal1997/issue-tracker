1b6461550be264d1a4b721ef260e8bb4bef98b87
Gal1997 <Galisraeli97@gmail.com>
2024-08-01 19:54:05 +0300

    API : Added POST() for creating 'issue' in database

---
+++ app/api/issues/route.tsx
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+interface issue {
+  title: string;
+  description: string;
+}
+
+const schema = z.object({
+  title: z.string().min(3).max(255),
+  description: z.string().min(3),
+});
+
+export async function POST(request: NextRequest) {
+  const body: issue = await request.json();
+  const validate = schema.safeParse(body);
+  if (!validate.success)
+    return NextResponse.json({ error: validate.error.errors }, { status: 400 });
+
+  const issue = await prisma.issue.findUnique({ where: { title: body.title } });
+  if (issue) {
+    return NextResponse.json(
+      { error: "Issue with this title already exists" },
+      { status: 409 }
+    );
+  }
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
