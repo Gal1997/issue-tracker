16dda62f5d858346084242817c050d78f2aebcfd
Gal1997 <Galisraeli97@gmail.com>
2024-08-05 17:26:23 +0300

    Handle error on submit issue

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -8,7 +8,7 @@ interface issue {
 }
 
 const schema = z.object({
-  title: z.string().min(3).max(255),
+  title: z.string().min(3, "Title length must greater than 3").max(255),
   description: z.string().min(3),
 });
 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { AiFillExclamationCircle } from "react-icons/ai";
 
 interface IssueForm {
   title: string;
@@ -14,15 +16,28 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   return (
     <form
       className="max-w-xl space-y-3"
       onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
+        try {
+          await axios.post("/api/issues", data);
+          router.push("/issues");
+        } catch (error) {
+          setError("An unexpected error occurred");
+        }
       })}
     >
+      {error && (
+        <Callout.Root color="red" role="alert">
+          <Callout.Icon>
+            <AiFillExclamationCircle />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
       <TextField.Root
         placeholder="Title"
         {...register("title")}
