dd4d32448e705a35fdc2870c14c80ce6b875567d
Gal1997 <Galisraeli97@gmail.com>
2024-08-09 01:17:38 +0300

    Error Handling: in case DELETE request fails, show alert dialog.

--- app/issues/[issueID]/DeleteIssueButton.tsx
+++ app/issues/[issueID]/DeleteIssueButton.tsx
@@ -1,12 +1,24 @@
 "use client";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { FaRegTrashCan } from "react-icons/fa6";
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
 const DeleteIssueButton = ({ issueID }: { issueID: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  async function deleteIssue() {
+    try {
+      throw new Error();
+      await axios.delete(`/api/issues/${issueID}`);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  }
   return (
     <div>
       <AlertDialog.Root>
@@ -24,25 +36,36 @@ const DeleteIssueButton = ({ issueID }: { issueID: number }) => {
 
           <Flex width="220px" mt="5" justify="between">
             <AlertDialog.Cancel>
-              <Button variant="soft" color="gold">
+              <Button variant="soft" color="gray">
                 Cancel
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-              <Button
-                variant="solid"
-                color="red"
-                onClick={async () => {
-                  await axios.delete(`/api/issues/${issueID}`);
-                  router.push("/issues");
-                  router.refresh();
-                }}
-              >
+              <Button variant="solid" color="red" onClick={deleteIssue}>
                 Delete
               </Button>
             </AlertDialog.Action>
           </Flex>
         </AlertDialog.Content>
+        <AlertDialog.Root open={error}>
+          <AlertDialog.Content maxWidth="400px">
+            <AlertDialog.Title>Error</AlertDialog.Title>
+            <AlertDialog.Description size="2">
+              {"Unable to remove issue. (Probably back-end issue)"}
+            </AlertDialog.Description>
+
+            <AlertDialog.Cancel>
+              <Button
+                variant="soft"
+                color="gray"
+                onClick={() => setError(false)}
+                mt="3"
+              >
+                Back
+              </Button>
+            </AlertDialog.Cancel>
+          </AlertDialog.Content>
+        </AlertDialog.Root>
       </AlertDialog.Root>
     </div>
   );
