45f2568bc2a901078e3a50d0856537f7a04a9b51
Gal1997 <Galisraeli97@gmail.com>
2024-08-11 17:39:31 +0300

    Changed NavBar layout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classname from "classnames";
 import { useSession } from "next-auth/react";
+import { Box, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
   const { status, data: session } = useSession();
@@ -21,33 +22,36 @@ const NavBar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
-    {
-      label: getStatus()[0],
-      href: getStatus()[1],
-    },
   ];
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 py-5">
-      <Link href="/">
-        <AiFillBug size={30} />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={links.indexOf(link)}>
-            <Link
-              href={link.href}
-              className={classname({
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <nav className="border-b mb-5 px-5 py-4">
+      <Flex justify="between">
+        <Flex className="space-x-6">
+          <Link href="/">
+            <AiFillBug size={30} />
+          </Link>
+          <ul className="flex space-x-6">
+            {links.map((link) => (
+              <li key={links.indexOf(link)}>
+                <Link
+                  href={link.href}
+                  className={classname({
+                    "text-zinc-900": link.href === currentPath,
+                    "text-zinc-500": link.href !== currentPath,
+                    "hover:text-zinc-800 transition-colors": true,
+                  })}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Flex>
+        <Flex mr={"0rem"}>
+          <Link href={getStatus()[1]}>{getStatus()[0]}</Link>
+        </Flex>
+      </Flex>
     </nav>
   );
 };
