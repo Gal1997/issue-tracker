5bd1533415d6924f595227754732f6890e4418e2
Gal1997 <Galisraeli97@gmail.com>
2024-08-14 18:52:41 +0300

    Fetched data from API to populate assign button with all registered users

--- app/api/issues/[id]/AssigneeSelect.tsx
+++ app/api/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,33 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+
+    // Call the function
+    fetchData();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." style={{ width: "100%" }} />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Gal Israeli</Select.Item>
+          {users.map((user) => (
+            <Select.Item value={user.id} key={user.id}>
+              {user.email}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
