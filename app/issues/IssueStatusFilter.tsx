e7c25445fc5b5cb8390a58fc52cfa16a2226904f
Gal1997 <Galisraeli97@gmail.com>
2024-08-21 17:49:51 +0300

    Fixed filtering bugs

--- app/issues/IssueStatusFilter.tsx
+++ app/issues/IssueStatusFilter.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value: Status | " " }[] = [
@@ -12,12 +12,18 @@ const statuses: { label: string; value: Status | " " }[] = [
 ];
 
 const IssueStatusFilter = () => {
+  const searchParams = useSearchParams();
   const router = useRouter();
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(status) => {
-        // We defined query to make sure status is defined and valid
-        const query = status != " " ? "?status=" + status : "";
+        const params = new URLSearchParams();
+        if (status != " ") params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+
+        const query = params.size > 0 ? "?" + params.toString() : "";
         router.push("issues/" + query);
       }}
     >
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -75,7 +75,10 @@ const Issues = async ({ searchParams }: Props) => {
             <Table.Row key={issue.id}>
               <Table.Cell>
                 <Link href={"/issues/" + issue.id}>{issue.title}</Link>
-                <div className="block md:hidden">{issue.status}</div>
+                <div className="block md:hidden">
+                  {" "}
+                  <IssueStatusBadge status={issue.status} />
+                </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
                 <IssueStatusBadge status={issue.status} />
