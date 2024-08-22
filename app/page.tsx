fa310e26058ba2f03a50217a765802e4d12fb050
Gal1997 <Galisraeli97@gmail.com>
2024-08-22 18:37:02 +0300

    Implementing pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+"use client";
 import Image from "next/image";
 import Pagination from "./components/Pagination";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
-  return <Pagination itemCount={100} pageSize={10} currentPage={1} />;
+  const queryParameters = useSearchParams();
+  return (
+    <Pagination
+      itemCount={100}
+      pageSize={10}
+      currentPage={parseInt(queryParameters.get("page")!)}
+    />
+  );
 }
