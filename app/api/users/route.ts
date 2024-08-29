b2c8ab5bf6d8a8c5e60bded530f68a172ae08f44
Gal1997 <Galisraeli97@gmail.com>
2024-08-29 03:51:34 +0300

    Minor fixes

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,8 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(reqeust: NextRequest) {
+export async function GET(request: NextRequest) {
   const allUsers = await prisma.user.findMany({ orderBy: { name: "asc" } });
-  return NextResponse.json(allUsers);
+  return NextResponse.json(request.json());
 }
+export const dynamic = "force-dynamic";
