5b6cae34008bee438cd674e9fbf91f27e7700f0d
Gal1997 <Galisraeli97@gmail.com>
2024-08-29 03:59:13 +0300

    Adj

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,8 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const allUsers = await prisma.user.findMany({ orderBy: { name: "asc" } });
   return NextResponse.json(allUsers);
 }
+export const dynamic = "force-dynamic";
